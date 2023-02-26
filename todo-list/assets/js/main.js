1c22c96ca42e5bd2a56f112529553ba0a378d92b
neotic <meirellez16@gmail.com>
2023-02-26 13:29:58 -0300

    added "language" button

--- todo-list/assets/js/main.js
+++ todo-list/assets/js/main.js
@@ -56,3 +56,42 @@ function updateLS(){
 
     localStorage.setItem('todos', JSON.stringify(todos))
 }
+
+
+const   button = document.querySelector('.language-button'),
+        pageTitle = document.querySelector('title'),
+        mainTitle = document.querySelector('.main-title'),
+        inputPlaceholder = document.querySelector('.form-input'),
+        instructionText1 = document.querySelector('[data-itext-1]'),
+        instructionText2 = document.querySelector('[data-itext-2]'),
+        instructionBoldText1 = document.querySelector('[data-bold-1]'),
+        instructionBoldText2 = document.querySelector('[data-bold-2]')
+
+
+button.addEventListener('click', () => {
+    const language = button.classList
+
+    if(language.contains('english')){
+        language.remove('english')
+        language.add('brazilian')
+        button.textContent = 'Português'
+        pageTitle.textContent = 'Lista de Tarefas'
+        mainTitle.textContent = 'Tarefas ✏️'
+        inputPlaceholder.placeholder = 'digite sua tarefa'
+        instructionText1.textContent = 'para marcar como concluido.'
+        instructionBoldText1.textContent = 'Botão Esquerdo'
+        instructionText2.textContent = 'para excluir tarefa.'
+        instructionBoldText2.textContent = 'Botão Direito'
+    } else {
+        language.remove('brazilian')
+        language.add('english')
+        button.textContent = 'English'
+        pageTitle.textContent = 'To-do list'
+        mainTitle.textContent = 'To-do list ✏️'
+        inputPlaceholder.placeholder = 'enter your to-do'
+        instructionText1.textContent = 'to toggle completed.'
+        instructionBoldText1.textContent = 'Left Click'
+        instructionText2.textContent = 'to delete todo.'
+        instructionBoldText2.textContent = 'Right Click'
+    }
+})
